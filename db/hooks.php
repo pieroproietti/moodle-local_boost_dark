9bdc5e1df4937df3fa866a278c85c54b21b31ac7
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-01 21:40:27 -0300

    Boilerplate Comment

--- db/hooks.php
+++ db/hooks.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of the Local Analytics plugin for Moodle
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -11,7 +11,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a boost_dark of the GNU General Public License
+// You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
