9f7c415f1f7e9b6b2e70f8499dbad225aa85c554
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-01 22:52:02 -0300

    Lib.php

---
+++ lib.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * lib.php
+ *
+ * @package   local_boost_dark
+ * @copyright 2024 Eduardo Kraus {@link http://eduardokraus.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Function local_boost_dark_add_htmlattributes
+ *
+ * @return array
+ */
+function local_boost_dark_add_htmlattributes() {
+    $layout = get_user_preferences("layout", "light");
+    $layouturl = optional_param("layout", false, PARAM_TEXT);
+    if ($layouturl) {
+        $layout = $layouturl;
+        set_user_preference("layout", $layout);
+    }
+    return ['data-bs-theme' => $layout];
+}
