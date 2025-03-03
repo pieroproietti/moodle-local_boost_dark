05c07505c12bb56678a4d288e2be7aa68f680e02
Eduardo Kraus <kraus@eduardokraus.com>
2025-03-03 14:18:13 -0300

    V 1.2.8

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025030100;
+$plugin->version   = 2025030300;
 $plugin->requires  = 2022041900;
-$plugin->release   = "1.2.7";
+$plugin->release   = "1.2.8";
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = "local_boost_dark";
